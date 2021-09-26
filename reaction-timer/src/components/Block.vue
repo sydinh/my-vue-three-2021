5268507669a90f93dcce49ce61472d6be7bd87c4
Sy Dinh <s.dinh@shopware.com>
2021-09-26 20:43:51 +0700

    Add reaction-timer

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="showBlock" class="block" @click="stopTimer">Click me</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        };
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer);
+            this.$emit('end', this.reactionTime);
+        },
+    },
+};
+</script>
+
+<style>
+.block {
+    margin: 10px;
+    padding: 10px;
+    color: #fff;
+    background: #444;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+    <p>Reaction time: {{ score }} ms</p>
+    <h1>Rank: {{ rank }}</h1>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+        this.rank = this.getRank(this.score);
+    },
+    methods: {
+        getRank(score) {
+            if (score < 250) {
+                return 'Saarik Fingers';
+            } else if (score < 400) {
+                return 'Rapid Reflexes';
+            } else {
+                return 'Finger Master';
+            }
+        },
+    },
+};
+</script>
+
+<style></style>
